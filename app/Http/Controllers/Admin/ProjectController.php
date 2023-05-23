e843ed7f5752b204af7cf4a21e91fb1d7c75dad9
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-05-24 00:32:00 +0200

    Add: create and store

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
@@ -26,7 +29,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.projects.create');
     }
 
     /**
@@ -35,9 +38,19 @@ class ProjectController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreProjectRequest $request)
     {
-        //
+
+        $data = $request->validated();
+        $newProject = new Project();
+        $newProject->fill($data);
+        $newProject->slug = Str::slug($data['title']);
+        if(isset($data['image'])){
+            $newProject->image = Storage::put('uploads', $data['image']);
+        }
+        $newProject->save();
+        return redirect()->route('admin.projects.index');
+
     }
 
     /**
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,3 +8,15 @@ import * as bootstrap from 'bootstrap'
 import.meta.glob([
     '../img/**'
 ])
+
+
+const imageInput = document.getElementById('image');
+
+imageInput.addEventListener('change', (e)=>{
+    const fileWrapper = document.querySelector('#file-wrapper');
+    fileWrapper.classList.remove('d-none');
+    fileWrapper.classList.add('d-block');
+    const src = URL.createObjectURL(e.target.files[0]);
+    const image = fileWrapper.firstElementChild;
+    image.src = src;
+})
